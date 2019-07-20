2d60867d6edbe8a033c97d6faa3a81010dd04b05
Mr.Lang <langzhiyao@126.com>
2019-07-20 18:24:53 +0800

    吸怪

--- application/admin/controller/Classes.php
+++ application/admin/controller/Classes.php
@@ -218,11 +218,9 @@ class Classes extends AdminControl {
                             $this->error('编辑失败','Classes/edit?class_id="'.$class_id.'"');
                         }
                     }else if(empty($res['position_id'])){
-                        $now = db('position')->where(array('position_id'=>$position_id))->update(array('is_bind'=>2,'create_time'=>time()));
+                         db('position')->where(array('position_id'=>$position_id))->update(array('is_bind'=>2,'create_time'=>time()));
                         $result = $model_class->editClass($data,array('classid'=>$class_id));
-                        var_dump($now);echo '<br/>';
-                        halt($result);
-                        if ($result && $now) {
+                        if ($result) {
                             $model_class->commit();
                             $this->success('编辑成功', 'Classes/index');
                         } else {
